0b7f9ff2b3ca74f69d68033929819ae18b4563d0
manuelvisuales <120177542+manuelvisuales@users.noreply.github.com>
2022-12-10 13:43:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
--let nombre=prompt("ingrese nombre");
+let nombre=prompt("ingrese nombre");
 if (nombre=="laura")
    {document.write("vienvenida laura");}
 else {document.write("usted no esta registrado");}
